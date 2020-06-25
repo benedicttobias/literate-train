b4b7509fe31739e8ba8a7dcf74e3c411314fdc81
Benedict Tobias <benedict.tobias@yahoo.com>
2020-06-25 09:05:41 -0400

    example of dummy vs smart components

--- src/containers/localePicker.js
+++ src/containers/localePicker.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { setLocale } from '../actions/localeActions'
+import Label from '../components/label';
 
 const LocalePicker = props => {
     const localeState = useSelector(state => state.LocaleReducer);
     const dispatch = useDispatch();
     return (
         <>
-            <span>{localeState.locale}</span>
+            <Label text={localeState.locale} />
             <button onClick={() => dispatch(setLocale('en'))}>English</button>
             <button onClick={() => dispatch(setLocale('es'))}>Spanish</button>
         </>
--- src/containers/themePicker.js
+++ src/containers/themePicker.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector, shallowEqual, useDispatch } from 'react-redux';
 import { setTheme } from '../actions/themeActions';
+import Label from '../components/label';
 
 const ThemePicker = props => {
     const themeState = useSelector(state => state.ThemeReducer, shallowEqual);
@@ -8,7 +9,7 @@ const ThemePicker = props => {
 
 return (
     <>
-        <span>{themeState.theme}</span>
+        <Label text={themeState.theme} />
         <button onClick={() => themeDispatch(setTheme('light'))}>Light</button>
         <button onClick={() => themeDispatch(setTheme('dark'))}>Dark</button>
     </>
