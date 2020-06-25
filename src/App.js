d8c508c998a2040bf478eaaba1b54f8ca2bd1fc7
Benedict Tobias <benedict.tobias@yahoo.com>
2020-06-24 22:26:13 -0400

    use provider and hooks to provide theme state

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React from "react";
 import ThemePicker from "./themePicker";
 import Store from "./store";
 import LocalePicker from "./localePicker";
+import { Provider } from 'react-redux';
 
 const App = props => {
   const store = Store;
 
   return (
-    <>
-      <ThemePicker store={store} />
+    <Provider store={store}>
+      <ThemePicker />
       <LocalePicker store={store} />
-    </>
+    </Provider>
   )
 }
 
--- src/themePicker.js
+++ src/themePicker.js
@@ -1,19 +1,17 @@
-import React, { useState } from 'react';
-import {connect} from 'react-redux';
+import React from 'react';
+import { useSelector, shallowEqual, useDispatch } from 'react-redux';
 
-let ThemePicker = props => {
-    const store = props.store;
-    const [theme] = useState(store.getState().theme);
+const ThemePicker = props => {
+    const themeState = useSelector(state => state.ThemeReducer, shallowEqual);
+    const themeDispatch = useDispatch();
 
-    return (
-        <>
-            <span>{theme}</span>
-            <button onClick={() => store.dispatch({type: 'SET_THEME', payload: 'light'})}>Light</button>
-            <button onClick={() => store.dispatch({type: 'SET_THEME', payload: 'dark'})}>Dark</button>
-        </>
-    )
+return (
+    <>
+        <span>{themeState.theme}</span>
+        <button onClick={() => themeDispatch({type:'SET_THEME', payload: 'light'})}>Light</button>
+        <button onClick={() => themeDispatch({type:'SET_THEME', payload: 'dark'})}>Dark</button>
+    </>
+)
 }
-
-ThemePicker = connect()(ThemePicker);
 
 export default ThemePicker;
